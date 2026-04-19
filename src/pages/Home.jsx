97f77cf0a01211e0496e90c5d4f115909bd912aa
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2026-04-19 16:47:09 +0530

    Updated files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -116,7 +116,7 @@ function Home() {
           </div>
         </section>
 
-        <section className="text-center space-y-4 mt-10">
+        <section className="border-t border-[#d3c7bb]/40 text-center space-y-4 mt-20">
           <h2 className="text-3xl sm:text-4xl font-semibold text-[#111111] font-serifHeading">
             Share Your Experience
           </h2>
